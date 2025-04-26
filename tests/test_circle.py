1915a7c91cc16629048f8aa9f86c5115ae522c4c
creaexo <creaexo@gmail.com>
2025-04-26 21:50:52 +0500

    refactor(test): Отредактированы тесты круга.
    Добавлен комментарий к ассёрту, изменён тайпхинт, убран необязательный вызов метода.

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -6,10 +6,10 @@ from area_calc.shatpes import Circle
 
 
 @pytest.mark.parametrize('radius', range(100))
-def test_circle_area(radius: int):
-    assert Circle(radius).area() == math.pi * radius ** 2
+def test_circle_area(radius: float):
+    assert Circle(radius).area() == math.pi * radius ** 2, "Неправильная площадь круга"
 
 
 def test_negative_radius_rais_value_error():
     with pytest.raises(ValueError):
-        Circle(-137).area()
+        Circle(-137)
