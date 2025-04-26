8ef57b7b13a95f138be6d3d9397c4baba5e59567
creaexo <creaexo@gmail.com>
2025-04-26 22:56:12 +0500

    refactor: Указаны абсолютные пути.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
-from shape_master import *
+from shape_master.shapes import *
+from shape_master.interface import *
 
 __all__ = ["Shape", "Shape2d", "Circle", "Triangle"]
--- shape_master/interface/__init__.py
+++ shape_master/interface/__init__.py
@@ -1,4 +1,5 @@
-from shape import Shape
-from shape_2d import Shape2d
-from shape_3d import Shape3d
+from shape_master.interface.shape import Shape
+from shape_master.interface.shape_2d import Shape2d
+from shape_master.interface.shape_3d import Shape3d
+
 __all__ = ['Shape', 'Shape2d', 'Shape3d']
