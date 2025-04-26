26714570e2e49225fdfd0bff6d46242d0d9d3677
creaexo <creaexo@gmail.com>
2025-04-26 19:26:44 +0500

    feat: Добавлен Треугольник.

--- area_calc/shatpes.py
+++ area_calc/shatpes.py
@@ -12,3 +12,26 @@ class Circle(Shape):
 
     def area(self) -> float:
         return math.pi * self.radius ** 2
+
+
+class Triangle(Shape):
+    """Треугольник"""
+    def __init__(self, a: float, b: float, c: float):
+        min_, middle_, max_ = sorted((a, b, c))
+        if min_ <= 0:
+            raise ValueError("Все стороны должны быть больше нуля")
+        elif min_ + middle_ <= max_:
+            raise ValueError("Сумма любых двух сторон должна быть больше третей")
+
+        self.a = a
+        self.b = b
+        self.c = c
+        self._is_rectangular = min_ ** 2 + middle_ ** 2 == max_ ** 2
+
+    def area(self) -> float:
+        s = (self.a + self.b + self.c) / 2
+        return math.sqrt(s * (s - self.a) * (s - self.b) * (s - self.c))
+
+    def is_rectangular(self) -> bool:
+        """Является ли треугольник прямоугольным."""
+        return self._is_rectangular
