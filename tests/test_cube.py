2ef5091c1bede1bc5abc5d2e8787cff7720e9c01
creaexo <creaexo@gmail.com>
2025-04-26 22:56:35 +0500

    test: Добавлен тест куба.

---
+++ tests/test_cube.py
@@ -0,0 +1,18 @@
+import pytest
+
+from shape_master.shapes.cube import Cube
+
+
+@pytest.mark.parametrize('side', range(1, 100))
+def test_circle_area(side: float):
+    assert Cube(side).area() == 6 * side ** 2, "Неожиданная площадь куба"
+
+
+@pytest.mark.parametrize('side', range(1, 100))
+def test_circle_area(side: float):
+    assert Cube(side).volume() == side ** 3, "Неожиданный объём куба"
+
+
+def test_negative_radius_rais_value_error():
+    with pytest.raises(ValueError):
+        Cube(-137)
